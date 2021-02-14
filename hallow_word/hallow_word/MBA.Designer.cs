8d6c2bbc71709584f7667d2902da4442041eb4a0
Mohmed Bensharrada <Mohmed Bensharrada@DESKTOP-P6B7QVH>
2021-02-14 10:42:05 +0200

    MBA change

--- hallow_word/hallow_word/MBA.Designer.cs
+++ hallow_word/hallow_word/MBA.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace hallow_word
 {
-    partial class Mohammed
+    partial class MBA
     {
         /// <summary>
         /// Required designer variable.
@@ -35,20 +35,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(122, 107);
+            this.label1.Location = new System.Drawing.Point(117, 101);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.Size = new System.Drawing.Size(45, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Mohammed Mansour";
+            this.label1.Text = "Hallow";
             // 
-            // Mohammed
+            // MBA
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.label1);
-            this.Name = "Mohammed";
-            this.Text = "Mohammed";
+            this.Name = "MBA";
+            this.Text = "MBA";
             this.ResumeLayout(false);
             this.PerformLayout();
 
